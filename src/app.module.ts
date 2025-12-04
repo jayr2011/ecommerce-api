0a9c58d368c0091ac60dc7f059c01644ca25c2e9
Jair Costa <juniorceia1996@gmail.com>
2025-12-04 11:04:44 -0300

    feat(categories): remover módulo, controlador e serviços de categorias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
 import { CartModule } from './cart/cart.module';
 import { OrdersModule } from './orders/orders.module';
@@ -22,7 +21,6 @@ import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
     }),
     AuthModule,
     UsersModule,
-    CategoriesModule,
     ProductsModule,
     CartModule,
     OrdersModule,
